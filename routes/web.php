74c0411c842507bb99760b0d33ef5acdbe47e718
龔育晴 <ching85117@gmail.com>
2017-10-25 20:29:36 +0800

    設定另一個 Route 以群組包起來設定 prefix

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,8 @@ Route::get('say/{name?}', ['as' => 'hello.index', function($name = 'Everybody') 
     return 'Hello, '.$name;
 }]);
 
-Route::get('dashboard', function() {
-    return 'dashboard';
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('dashboard', function() {
+        return 'admin dashboard';
+    });
 });
