bc183ecc5335bdac07bd346dbdcac8b02d7d409f
龔育晴 <ching85117@gmail.com>
2017-10-25 20:08:00 +0800

    修改參數成選擇性

--- routes/web.php
+++ routes/web.php
@@ -11,14 +11,10 @@
 |
 */
 
-Route::get('/', function() {
-    return redirect('welcome');
+Route::get('/',function(){
+   return redirect('welcome');
 });
 
-Route::get('hello/{name}', function($name) {
-    return 'Hello, '.$name;
-});
-
-Route::get('hello/{name?}', function($name = 'Everybody') {
-return 'Hello, '.$name;
+Route::get('hello/{name?}', function($name ='Everybody'){
+   return 'Hello,'.$name;
 });
